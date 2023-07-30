5f3e97f79f73c4fda24534ef1fe655b6340d06d9
iKalashnikova <ikalashnikova@ukr.net>
2023-07-30 17:42:28 +0200

    .

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -1,13 +1,13 @@
 import express from "express";
-import authConroller from "../../controller/auth-controller.js";
+import authController from "../../controller/auth-controller.js";
 import authenticate from "../../middlewars/authenticate.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/users/register', authConroller.signup );
+authRouter.post('/users/register', authController.signup );
 
-authRouter.post('/users/login', authConroller.signin);
+authRouter.post('/users/login', authController.signin);
 
-authRouter.get('/current', authenticate, authConroller.getCurrent );
+authRouter.get('/current', authenticate, authController.getCurrent);
 
 export default authRouter;
